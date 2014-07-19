f763a1a38c54316d881a1f0ca9185da4d03184f8
Rohan Isaac <rohan_isaac@yahoo.com>
2014-07-19 18:31:23 -0400

    Reorganized processing, streamlined extracting data including label information and subfiles

--- driver.py
+++ driver.py
@@ -7,7 +7,17 @@ Driver function for spc file
 
 import spc
 
-ftest = spc.File('Data/S_EVENX.SPC')
-print ftest.x_values
-print dir(ftest)
-print ftest.metadict
+ftest = spc.File('Data/HENE25.SPC')
+#print ftest.x_values
+for i in dir(ftest):
+    print i
+#print ftest.metadict
+    
+print ftest.dat.y
+print ftest.x
+
+print len(ftest.dat.y)
+
+print len(ftest.x)
+
+print ftest.log_dict
--- spc.py
+++ spc.py
@@ -29,6 +29,29 @@ Plan for the future
 -Subfile decodes the header and the data for each subfile
 
 
+* Thus an SPC trace file normally has these components in the following order:
+*	SPCHDR		Main header (512 bytes in new format, 224 or 256 in old)
+*      [X Values]	Optional FNPTS 32-bit floating X values if TXVALS flag
+*	SUBHDR		Subfile Header for 1st subfile (32 bytes)
+*	Y Values	FNPTS 32 or 16 bit fixed Y fractions scaled by exponent
+*      [SUBHDR	]	Optional Subfile Header for 2nd subfile if TMULTI flag
+*      [Y Values]	Optional FNPTS Y values for 2nd subfile if TMULTI flag
+*	...		Additional subfiles if TMULTI flag (up to FNSUB total)
+*      [Log Info]	Optional LOGSTC and log data if flogoff is non-zero
+
+* However, files with the TXYXYS ftflgs flag set have these components:
+*	SPCHDR		Main header (512 bytes in new format)
+*	SUBHDR		Subfile Header for 1st subfile (32 bytes)
+*	X Values	FNPTS 32-bit floating X values
+*	Y Values	FNPTS 32 or 16 bit fixed Y fractions scaled by exponent
+*      [SUBHDR	]	Subfile Header for 2nd subfile
+*      [X Values]	FNPTS 32-bit floating X values for 2nd subfile
+*      [Y Values]	FNPTS Y values for 2nd subfile
+*	...		Additional subfiles (up to FNSUB total)
+*      [Directory]	Optional FNSUB SSFSTC entries pointed to by FNPTS
+*      [Log Info]	Optional LOGSTC and log data if flogoff is non-zero
+
+
 """
 
 # need to functionalize 
@@ -58,15 +81,17 @@ class File:
     # --------------------------------------------
     # calculate size of strings using `struct.calcsize(string)`
     head_str = "<cccciddicccci9s9sh32s130s30siicchf48sfifc187s"
-    old_head_str = "<ccifffccicccc8sii28s130s30s32s"
+    #old_head_str = "<ccifffccicccc8sii28s130s30s32s"
+    logstc_str = "<iiiii44s"
+    print "Size of struct lgo" , struct.calcsize(logstc_str)    
     
     # byte positon of various parts of the file
     # --------------------------------------------
     head_siz = 512
     subhead_siz = 32
+    log_siz = 64
     
-    HEAD_POS = 512
-    SUBHEAD_POS = 544
+    subhead1_pos = head_siz + subhead_siz
     
     # --------------------------------------------
     
@@ -138,41 +163,76 @@ class File:
         #--------------------------
         
         self.fnpts = int(self.fnpts)
+        self.fexp = ord(self.fexp)
+        
         self.ffirst = int(self.ffirst)
         self.flast = int(self.flast)
+        
+        self.flogoff = int(self.flogoff)
+        
+        self.fxtype = ord(self.fxtype)
+        self.fytype = ord(self.fytype)
+        self.fztype = ord(self.fztype)
+        
+        #--------------------------
+        # options
+        #--------------------------            
+        
+        # optional floating point x-values
+        if self.txvals: 
+            print "Seperate x-values"
+        else:
+            print "Generated x-values"
+            self.x = np.linspace(self.ffirst,self.flast,num=self.fnpts)
+        
+        # multiple y values
+        if self.tmulti: 
+            print "Multiple y-values"
+            self.sub = []
+            # to be implemented
+            #for i in range(self.fnsub):
+            #    self.sub.append(subFile(content[:],self.pr_spacing))   
+        else: # single y values
+            print "Single set of y-values"
+            y_dat_pos = self.head_siz
+            y_dat_pos_end = self.head_siz + self.subhead_siz + (4*self.fnpts)
+            # each data point is 4 bytes long
+            self.dat = subFile(content[y_dat_pos:y_dat_pos_end],self.fnpts,self.fexp)
+            
+        # flog offset to log data offset not zero (bytes)
+        if self.flogoff: 
+            print "Log data exists"
+            log_pos = self.flogoff + self.log_siz
+            self.logsizd, \
+                self.logsizm, \
+                self.logtxto, \
+                self.logbins, \
+                self.logdsks, \
+                self.logspar \
+                = struct.unpack(self.logstc_str, content[self.flogoff:log_pos])
+                
+            #print "log stuff", self.logsizd, self.logsizm
+            log_end_pos = log_pos + self.logsizd
+            self.log_content = content[log_pos:log_end_pos].split('\r\n')
+            
+            # split log data into dictionary
+            self.log_dict = dict()
+            for x in self.log_content:
+                if x.find('=') >= 0:
+                    key, value = x.split('=')
+                    self.log_dict[key] = value
              
+        
         #--------------------------
         # spacing between data
         #--------------------------
 
         self.pr_spacing = (self.flast-self.ffirst)/(self.fnpts-1)
+        
+        
+    def process_log(self):
+        """ Process log data, split into log dict """
 
-        
-
-        
-        #--------------------------
-        # loop over subfiles
-        #--------------------------
-        
-        self.sub = []
-        for i in range(self.fnsub):
-            self.sub.append(subFile(content[:],self.pr_spacing))
-        
-        
-        #--------------------------
-        # find column headers
-        #--------------------------    
-        
-        if self.talabs:
-            # if supposed to use fcatxt, split it based on 00 string and only use the 
-            # first elements to get the x and y labels
-            [self.xl, self.yl] =  self.fcatxt.split('\x00')[:2]
-        else:
-            [self.xl, self.yl] = [self.xlabel,self.ylabel]
-        print self.xl, "\t", self.yl
-        for i in range(self.PTS):
-            print self.x_values[i], "\t", self.y_values_int[i]
-            #, y_values[i]
         
         
 
@@ -180,27 +240,134 @@ class File:
         # import metadata
         #--------------------------   
         
-        # Optional metadata, check if it exists first 
-        metastr = '[SCAN PARAM]\r\n'
-        metapos = content.find(metastr)
-        if metapos != -1:
-            metadata = content[metapos+len(metastr):]
-            metalst = metadata.split('\r\n')
-            keylst = []
-            vallst = []
-            for x in metalst[1:-1]:
-                [key,val] = x.split('=')
-                keylst.append(key)
-                vallst.append(val)
-            
-            self.metadict = dict(zip(keylst,vallst))
+#        # Optional metadata, check if it exists first 
+#        metastr = '[SCAN PARAM]\r\n'
+#        metapos = content.find(metastr)
+#        if metapos != -1:
+#            metadata = content[metapos+len(metastr):]
+#            metalst = metadata.split('\r\n')
+#            keylst = []
+#            vallst = []
+#            for x in metalst[1:-1]:
+#                [key,val] = x.split('=')
+#                keylst.append(key)
+#                vallst.append(val)
+#            
+#            self.metadict = dict(zip(keylst,vallst))
     
+    def set_labels(self):
+        """ 
+        Set the x, y, z axis labels using various information in file content 
+        """
+        
+        #--------------------------
+        # units for x,z,w axes
+        #--------------------------
+        fxtype_op = ["Arbitrary", \
+            "Wavenumber (cm-1)", \
+            "Micrometers (um)", \
+            "Nanometers (nm)", \
+            "Seconds ", \
+            "Minutes", "Hertz (Hz)", \
+            "Kilohertz (KHz)", \
+            "Megahertz (MHz) ", \
+            "Mass (M/z)", \
+            "Parts per million (PPM)", \
+            "Days", \
+            "Years", \
+            "Raman Shift (cm-1)", \
+            "eV", \
+            "XYZ text labels in fcatxt (old 0x4D version only)", \
+            "Diode Number", \
+            "Channel", \
+            "Degrees", \
+            "Temperature (F)",  \
+            "Temperature (C)", \
+            "Temperature (K)", \
+            "Data Points", \
+            "Milliseconds (mSec)", \
+            "Microseconds (uSec) ", \
+            "Nanoseconds (nSec)", \
+            "Gigahertz (GHz)", \
+            "Centimeters (cm)", \
+            "Meters (m)", \
+            "Millimeters (mm)", \
+            "Hours"]
+             
+        
+        if self.fxtype < 30:
+            self.pr_xlabel = fxtype_op[self.fxtype]
+        else:
+            self.pr_xlabel = "Unknown"
+            
+        if self.fztype < 30:
+            self.pr_zlabel = fxtype_op[self.fztype]
+        else:
+            self.pr_zlabel = "Unknown"            
+        
+            
+        #--------------------------
+        # units y-axis
+        #--------------------------
+        
+        fytype_op = ["Arbitrary Intensity", \
+            "Interferogram", \
+            "Absorbance", \
+            "Kubelka-Monk", \
+            "Counts", \
+            "Volts", \
+            "Degrees", \
+            "Milliamps", \
+            "Millimeters", \
+            "Millivolts", \
+            "Log(1/R)", \
+            "Percent", \
+            "Intensity", \
+            "Relative Intensity", \
+            "Energy", \
+            "", \
+            "Decibel", \
+            "", \
+            "", \
+            "Temperature (F)", \
+            "Temperature (C)", \
+            "Temperature (K)", \
+            "Index of Refraction [N]", \
+            "Extinction Coeff. [K]", \
+            "Real", \
+            "Imaginary", \
+            "Complex"]
+        
+        fytype_op2 = ["Transmission", \
+            "Reflectance", \
+            "Arbitrary or Single Beam with Valley Peaks",  \
+            "Emission" ]
+        
+        
+        if self.fytype < 27:
+            self.pr_ylabel = fytype_op[self.fytype]
+        elif self.fytype > 127 and self.fytype < 132:
+            self.pr_ylabel = fytype_op2[self.fytype - 128]
+        else:
+            self.pr_ylabel = "Unknown"   
+            
 
+            
+        #--------------------------
+        # check if labels are included as text
+        #--------------------------    
+        
+        # split it based on 00 string 
+        # format x, y, z
+        if self.talabs:
+            [self.pr_xlabel, self.pr_ylabel, self.pr_zlabel] =  self.fcatxt.split('\x00')[:3]
         
 
             
     def interpret(self):
-        """ Interpret flags and header information and extract useful data """
+        """ 
+        Interpret flags and header information and extract useful data 
+        """
         
         # Flag bits
         if self.tsprec:
@@ -267,94 +434,10 @@ class File:
         else:
             print "Multiple subfiles"
             
-        #--------------------------
-        # units for x,z,w axes
-        #--------------------------
-        fxtype_op = ["Arbitrary", \
-            "Wavenumber (cm-1)", \
-            "Micrometers (um)", \
-            "Nanometers (nm)", \
-            "Seconds ", \
-            "Minutes", "Hertz (Hz)", \
-            "Kilohertz (KHz)", \
-            "Megahertz (MHz) ", \
-            "Mass (M/z)", \
-            "Parts per million (PPM)", \
-            "Days", \
-            "Years", \
-            "Raman Shift (cm-1)", \
-            "eV", \
-            "XYZ text labels in fcatxt (old 0x4D version only)", \
-            "Diode Number", \
-            "Channel", \
-            "Degrees", \
-            "Temperature (F)",  \
-            "Temperature (C)", \
-            "Temperature (K)", \
-            "Data Points", \
-            "Milliseconds (mSec)", \
-            "Microseconds (uSec) ", \
-            "Nanoseconds (nSec)", \
-            "Gigahertz (GHz)", \
-            "Centimeters (cm)", \
-            "Meters (m)", \
-            "Millimeters (mm)", \
-            "Hours"]
-             
-        fxtype_ord = ord(self.fxtype)
-        if fxtype_ord < 30:
-            self.xlabel = fxtype_op[fxtype_ord]
-        else:
-            self.xlabel = "Unknown"
-        
-            
-        #--------------------------
-        # units y-axis
-        #--------------------------
-        
-        fytype_op = ["Arbitrary Intensity", \
-            "Interferogram", \
-            "Absorbance", \
-            "Kubelka-Monk", \
-            "Counts", \
-            "Volts", \
-            "Degrees", \
-            "Milliamps", \
-            "Millimeters", \
-            "Millivolts", \
-            "Log(1/R)", \
-            "Percent", \
-            "Intensity", \
-            "Relative Intensity", \
-            "Energy", \
-            "", \
-            "Decibel", \
-            "", \
-            "", \
-            "Temperature (F)", \
-            "Temperature (C)", \
-            "Temperature (K)", \
-            "Index of Refraction [N]", \
-            "Extinction Coeff. [K]", \
-            "Real", \
-            "Imaginary", \
-            "Complex"]
-        
-        fytype_op2 = ["Transmission", \
-            "Reflectance", \
-            "Arbitrary or Single Beam with Valley Peaks",  \
-            "Emission" ]
-        
-        fytype_ord = ord(self.fytype)
-        if fytype_ord < 27:
-            self.ylabel = fytype_op[fytype_ord]
-        elif fytype_ord > 127 and fytype_ord < 132:
-            self.ylabel = fytype_op2[fytype_ord - 128]
-        else:
-            self.ylabel = "Unknown"   
             
         print "There are ", self.fnpts, " points between ", self.ffirst, \
             " and ", self.flast, " in steps of ", self.pr_spacing
+        
             
         #--------------------------
         # file comment
@@ -363,6 +446,14 @@ class File:
         # is junk after that
         print str(self.fcmnt).split('\x00')[0]
         
+    def output_txt(self):
+        """ Output data as plain text, can feed to file later """
+        print self.pr_xlabel, "\t", self.pr_ylabel
+        
+        
+        for i in range(self.fnpts):
+            print self.x[i], "\t", self.dat.y[i]
+            #, y_values[i]
         
                 
     def print_metadata(self):
@@ -415,7 +506,7 @@ class subFile:
     subhead_str = "<cchfffiif4s"
     subhead_siz = 32
     
-    def __init__(self,data,spacing):
+    def __init__(self, data, fnpts, fexp):
         #--------------------------
         # decode subheader
         #--------------------------
@@ -432,7 +523,17 @@ class subFile:
             subresv \
             = struct.unpack(self.subhead_str, data[:self.subhead_siz])
             
-        self.pr_spacing = spacing
+        #--------------------------
+        # extract y_data
+        #--------------------------
+            
+        y_dat_str = 'i'*fnpts
+        y_raw = np.array(struct.unpack(y_dat_str, data[32:]))
+        
+        self.y = (2**(fexp-32))*y_raw
+            
+            
+        #self.pr_spacing = spacing
             
             
         # do stuff if subflgs
@@ -446,22 +547,22 @@ class subFile:
         
         # header + subheader + mumber of data points (int: 4 bytes)
         # ONLY VALID FOR SINGLE SUBFILES !!! need to fix
-        self.DATA_POS = 512 + 32 + (self.PTS*4)
-        self.EXP = ord(self.fexp)
-        self.PTS = int(self.fnpts)
-        
-        # generate x-values (np.arange can't generate the correct amount of elements)
-        self.x_values = np.zeros(self.PTS)
-        for i in range(self.PTS):
-            self.x_values[i] = self.ffirst + (self.SPACING*i)
-            
-        # import the y-data and convert it
-        self.y_int = np.array(struct.unpack("i"*self.PTS,data[self.subhead_siz:]))
-        
-        # conversion string
-        self.y_values = (2**(self.EXP-32))*self.y_int
-        
-        # optionally integerize it
-        self.y_values_int = self.y_values.astype(int)
+#        self.DATA_POS = 512 + 32 + (self.PTS*4)
+#        self.EXP = ord(self.fexp)
+#        self.PTS = int(self.fnpts)
+#        
+#        # generate x-values (np.arange can't generate the correct amount of elements)
+#        self.x_values = np.zeros(self.PTS)
+#        for i in range(self.PTS):
+#            self.x_values[i] = self.ffirst + (self.SPACING*i)
+#            
+#        # import the y-data and convert it
+#        self.y_int = np.array(struct.unpack("i"*self.PTS,data[self.subhead_siz:]))
+#        
+#        # conversion string
+#        self.y_values = (2**(self.EXP-32))*self.y_int
+#        
+#        # optionally integerize it
+#        self.y_values_int = self.y_values.astype(int)
             
         
