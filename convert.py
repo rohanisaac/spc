f47066125994a7f739056359602d44873618d575
Rohan Isaac <rohan_isaac@yahoo.com>
2014-08-23 15:51:57 -0400

    Conversion module for command line spc to text given dir/filename(s)

---
+++ convert.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Command line utility to convert .SPC files to .TXT
+
+@author: Rohan Isaac
+"""
+
+import sys, os, glob
+
+man = """ USAGE: \n $ python convert %file_name1% %file_name2% \n OR \n \
+$ python convert $%dir_name% \n\n EXAMPLE: python convert """
+
+def convert(filename):
+    pass
+
+def main():
+    if len(sys.argv) < 2:
+        print "No arguments passed"
+        print man
+        
+    else:
+        print "Converting the following files: "
+        for path in sys.argv[1:]:
+            print path
+            if os.path.isdir(path):
+                print "Do stuff if dir"
+                for filename in glob.glob(path + "/*.[sS][pP][cC]" ):
+                    convert(filename)
+            else:
+                convert(filename)
+
+if __name__ == "__main__":
+    main()
+
