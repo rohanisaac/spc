f3656b6c6e2040e882f1afffae068e2935b69e10
Rohan Isaac <rohan_isaac@yahoo.com>
2014-08-27 22:32:11 -0400

    Added non-functional menu

--- convert_gui.py
+++ convert_gui.py
@@ -16,19 +16,41 @@ class MainFrame(wx.Frame):
 
         btn1 = wx.Button(frame_pnl, label="Open files")      
         btn2 = wx.Button(frame_pnl, label="Open folder")
-
-        pnl_horSiz = wx.BoxSizer(wx.HORIZONTAL)
-        pnl_horSiz.AddStretchSpacer() # for v centering
-        pnl_horSiz.Add(btn1, flag=wx.ALIGN_CENTER) # for h centering
-        pnl_horSiz.Add(btn2, flag=wx.ALIGN_CENTER)
-        pnl_horSiz.AddStretchSpacer() # for vertical centering
-
-        frame_pnl.SetSizer(pnl_horSiz)
-        frame_pnl.Layout()
         
         # bind buttons to events
         self.Bind(wx.EVT_BUTTON, self.get_path, btn1)
         self.Bind(wx.EVT_BUTTON, self.get_dir, btn2)
+        
+        # Adds menubar
+        menubar = wx.MenuBar()
+        file = wx.Menu()
+        convert = wx.Menu()
+        
+        # adds items
+        file.Append(101, '&Open', 'Open a file')
+        file.Append(102, '&Save', 'Save the document')
+        file.AppendSeparator()
+        file.Append(105, '&Quit', 'Quit the application')
+        
+        menubar.Append(file, '&File')
+        menubar.Append(convert, '&Convert')
+        
+        self.SetMenuBar(menubar)
+        
+        #list  control
+        #lc = wx.ListCtrl(self, -1, style=wx.LC_REPORT)
+        #lc.InsertColumn(0, 'File Name')
+        
+        
+        pnl_horSiz = wx.BoxSizer(wx.HORIZONTAL)
+        pnl_horSiz.AddStretchSpacer() # for v centering
+        pnl_horSiz.Add(btn1, flag=wx.ALIGN_CENTER) # for h centering
+        pnl_horSiz.Add(btn2, flag=wx.ALIGN_CENTER)
+        
+        pnl_horSiz.AddStretchSpacer() # for vertical centering
+        #pnl_horSiz.Add(lc, flag=wx.ALIGN_CENTER)
+        frame_pnl.SetSizer(pnl_horSiz)
+        frame_pnl.Layout()
         
     def get_path(self, event):
         print "works path"
@@ -40,7 +62,7 @@ class MainFrame(wx.Frame):
         else:
             path = None
         dialog.Destroy()
-        return path
+        self.path = path
         
     def get_dir(self, event):
         print "works dir"
@@ -52,7 +74,7 @@ class MainFrame(wx.Frame):
         else:
             path = None
         dialog.Destroy()
-        return path
+        self.path = path
         
 if __name__ == '__main__' :
     app = wx.PySimpleApp( redirect=False)
