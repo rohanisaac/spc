64f6254af534239606d9affc2b848de120eda8bf
Rohan Isaac <rohan_isaac@yahoo.com>
2014-10-17 00:04:45 -0400

    Working setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 setup(name='spc',
       version='0.1',
-      py_modules=['spc'],
+      description='Load and convert binary spc spectroscopy files',
+      author='Rohan Isaac',
+      author_email='rohan_isaac@yahoo.com',
+      url='https://github.com/rohanisaac/spc',
+      packages=['spc'],
+      install_requires=['numpy','scipy','matplotlib'],
       )
