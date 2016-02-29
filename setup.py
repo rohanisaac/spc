e6a3472208415f01501d286fd18b4671f3da4f9a
Rohan Isaac <rohan_isaac@yahoo.com>
2016-02-28 19:40:08 -0500

    Updated setup, moved info to module

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+
+import spc
+
 setup(name='spc',
-      version='0.2',
-      description='Load and convert binary spc spectroscopy files',
-      author='Rohan Isaac',
-      author_email='rohan_isaac@yahoo.com',
+      version=spc.__version__,
+      description=spc.__doc__,
+      author=spc.__author__,
+      author_email=spc.__author_email__,
       url='https://github.com/rohanisaac/spc',
       packages=['spc'],
       install_requires=['numpy'],
--- spc/__init__.py
+++ spc/__init__.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python
 """
-author: Rohan Isaac
+Module for reading, exploring and converting SPC spectroscopic binary data in Python.
 """
 
-from spc import File
+from .spc import File
+
+__author__ = "Rohan Isaac"
+__author_email__ = "rohan_isaac@yahoo.com"
+__version__ = "0.3"
+__license__ = "GPLv3"
