a46a587f019b6b511f9e0406187fad6bb3bbdbe9
Rohan Isaac <rohan_isaac@yahoo.com>
2016-03-06 22:19:37 -0500

    Updated classifiers

--- setup.py
+++ setup.py
@@ -13,10 +13,11 @@ setup(name='spc',
       packages=['spc'],
       install_requires=['numpy'],
       classifiers=[
-        'Development Status :: 4 - Beta'
-        'Intended Audience :: Science/Research'
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Chemistry',
         ],
       )
